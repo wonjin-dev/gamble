3b913eec2d4472f1f2c90d93d6d3ab42cf01a474
wonjin-dev <fsdjin159@gmail.com>
2023-01-16 01:54:33 +0900

    ✅ gamble 완료 E2E 코드 작성

--- cypress/e2e/gamble.cy.ts
+++ cypress/e2e/gamble.cy.ts
@@ -9,4 +9,22 @@ describe('gamble', () => {
     cy.wait(600);
     cy.contains('다시 뽑기').click();
   });
+
+  it('결과 확인하기', () => {
+    cy.visit('http://localhost:3000');
+
+    for (let i = 0; i < 10; i++) {
+      cy.get(':nth-child(1) > .css-1i0retr').click();
+    }
+
+    for (let i = 0; i < 10; i++) {
+      cy.get(':nth-child(2) > .css-1i0retr').click();
+    }
+
+    for (let i = 0; i < 10; i++) {
+      cy.get(':nth-child(3) > .css-1i0retr').click();
+    }
+  });
 });
+
+export {};
