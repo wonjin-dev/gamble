f98b5f9cba9da2782f29236ae74bc228f57e451c
wonjin-dev <fsdjin159@gmail.com>
2023-01-16 01:59:37 +0900

    :recycle: E2E 코드 리팩터링

--- cypress/e2e/gamble.cy.ts
+++ cypress/e2e/gamble.cy.ts
@@ -1,29 +1,27 @@
+const completeGamble = (gambleClick: () => void) => {
+  for (let i = 0; i < 10; i++) {
+    gambleClick();
+  }
+};
+
 describe('gamble', () => {
   it('다시 뽑기', () => {
     cy.visit('http://localhost:3000');
 
-    for (let i = 0; i < 10; i++) {
-      cy.contains('강화').click();
-      cy.wait(100);
-    }
+    completeGamble(() => cy.contains('강화').click());
     cy.wait(600);
+
     cy.contains('다시 뽑기').click();
   });
 
   it('결과 확인하기', () => {
     cy.visit('http://localhost:3000');
 
-    for (let i = 0; i < 10; i++) {
-      cy.get(':nth-child(1) > .css-1i0retr').click();
-    }
+    completeGamble(() => cy.get(':nth-child(1) > .css-1i0retr').click());
 
-    for (let i = 0; i < 10; i++) {
-      cy.get(':nth-child(2) > .css-1i0retr').click();
-    }
+    completeGamble(() => cy.get(':nth-child(2) > .css-1i0retr').click());
 
-    for (let i = 0; i < 10; i++) {
-      cy.get(':nth-child(3) > .css-1i0retr').click();
-    }
+    completeGamble(() => cy.get(':nth-child(3) > .css-1i0retr').click());
   });
 });
 
