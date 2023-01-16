33b0c36859d38162d8187a34005f0e9b33ca26bd
wonjin-dev <fsdjin159@gmail.com>
2023-01-16 18:28:38 +0900

    :recycle: useGamble_enchant 중복 코드 리팩터링

--- src/hooks/gamble/useGamble.ts
+++ src/hooks/gamble/useGamble.ts
@@ -38,7 +38,7 @@ const useGamble = (abilities: AbilityType[]): GambleProps => {
   const {pbt, success, fail} = useProbability();
   const [positive1, setPositive1] = useRecoilState(positive1Atom);
   const [positive2, setPositive2] = useRecoilState(positive2Atom);
-  const [negative, setNeagtive] = useRecoilState(negativeAtom);
+  const [negative, setNegative] = useRecoilState(negativeAtom);
 
   const isOver = useMemo(
     () => positive1.score.length === 10 && positive2.score.length === 10 && negative.score.length === 10,
@@ -48,7 +48,7 @@ const useGamble = (abilities: AbilityType[]): GambleProps => {
   const init = () => {
     setPositive1({...positive1, ability: abilities[0]});
     setPositive2({...positive2, ability: abilities[1]});
-    setNeagtive({...negative, ability: abilities[2]});
+    setNegative({...negative, ability: abilities[2]});
   };
 
   useEffectOnce(init);
@@ -71,51 +71,38 @@ const useGamble = (abilities: AbilityType[]): GambleProps => {
   const enchant = useCallback(
     (section: GambleSectionList) => {
       const res = gamble(pbt);
+      const target = detail(section);
 
-      if (section === 'positive1') {
-        if (positive1.score.length < 10) {
-          if (res) {
-            setPositive1({...positive1, score: [...positive1.score, true]});
-            success();
-          } else {
-            setPositive1({...positive1, score: [...positive1.score, false]});
-            fail();
+      const attempt = (result: boolean) => {
+        if (target && target.score.length < 10) {
+          if (target === positive1) {
+            return setPositive1({...target, score: [...target.score, result]});
+          }
+          if (target === positive2) {
+            return setPositive2({...target, score: [...target.score, result]});
+          }
+          if (target === negative) {
+            return setNegative({...target, score: [...target.score, result]});
           }
         }
-      }
+      };
 
-      if (section === 'positive2') {
-        if (positive2.score.length < 10) {
-          if (res) {
-            setPositive2({...positive2, score: [...positive2.score, true]});
-            success();
-          } else {
-            setPositive2({...positive2, score: [...positive2.score, false]});
-            fail();
-          }
-        }
-      }
-
-      if (section === 'negative') {
-        if (negative.score.length < 10) {
-          if (res) {
-            setNeagtive({...negative, score: [...negative.score, true]});
-            success();
-          } else {
-            setNeagtive({...negative, score: [...negative.score, false]});
-            fail();
-          }
-        }
+      if (res) {
+        attempt(true);
+        success();
+      } else {
+        attempt(false);
+        fail();
       }
     },
-    [fail, negative, pbt, positive1, positive2, setNeagtive, setPositive1, setPositive2, success]
+    [detail, fail, negative, pbt, positive1, positive2, setNegative, setPositive1, setPositive2, success]
   );
 
   const reset = () => {
     const newAbilities = abilitiesGenerator();
     setPositive1({ability: newAbilities[0], score: []});
     setPositive2({ability: newAbilities[1], score: []});
-    setNeagtive({ability: newAbilities[2], score: []});
+    setNegative({ability: newAbilities[2], score: []});
   };
 
   return {detail, enchant, reset, isOver};
