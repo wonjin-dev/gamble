a5ca5dad775f6783a6c48e5810222ff9c8d92dc2
wonjin-dev <fsdjin159@gmail.com>
2023-01-16 17:44:22 +0900

    ✅ 능력 요소 랜덤 생성함수 테스트 코드 작성

--- src/__test__/generator.test.ts
+++ src/__test__/generator.test.ts
@@ -1,8 +1,15 @@
-import {gamble, randomNumberArrayGenerator, randomNumberGenerator} from "../utils/generators";
+import {AbilityType} from '@hooks/gamble/useGamble';
+import {
+  abilitiesGenerator,
+  abilityList,
+  gamble,
+  randomNumberArrayGenerator,
+  randomNumberGenerator,
+} from '../utils/generators';
 
 describe('[유틸] randomNumberGenerator', () => {
   it('0보다 크고, 특정 숫자 이하의 정수 값을 리턴', () => {
-    for(let i = 10; i < 1000; i++) {
+    for (let i = 10; i < 1000; i++) {
       const randomValue = randomNumberGenerator(i);
       expect(randomValue).toBeGreaterThanOrEqual(0);
       expect(randomValue).toBeLessThanOrEqual(i);
@@ -17,8 +24,8 @@ describe('[유틸] randomNumberArrayGenerator', () => {
 
       expect(randomNumberArr.length).toBe(i);
     }
-  })
-})
+  });
+});
 
 describe('[유틸] gamble', () => {
   it('25% 확률 검증, 오차 범위 1.5%', () => {
@@ -26,82 +33,95 @@ describe('[유틸] gamble', () => {
     const resultArr = [];
 
     for (let i = 0; i < 10000; i++) {
-      if(gamble(probability)) {
+      if (gamble(probability)) {
         resultArr.push(true);
       }
     }
 
     expect(resultArr.length).toBeGreaterThanOrEqual(2350);
     expect(resultArr.length).toBeLessThanOrEqual(2650);
-  })
+  });
 
   it('35% 확률 검증, 오차 범위 1.5%', () => {
     const probability = 35;
     const resultArr = [];
 
     for (let i = 0; i < 10000; i++) {
-      if(gamble(probability)) {
+      if (gamble(probability)) {
         resultArr.push(true);
       }
     }
 
     expect(resultArr.length).toBeGreaterThanOrEqual(3350);
     expect(resultArr.length).toBeLessThanOrEqual(3650);
-  })
+  });
 
   it('45% 확률 검증, 오차 범위 1.5%', () => {
     const probability = 45;
     const resultArr = [];
 
     for (let i = 0; i < 10000; i++) {
-      if(gamble(probability)) {
+      if (gamble(probability)) {
         resultArr.push(true);
       }
     }
 
     expect(resultArr.length).toBeGreaterThanOrEqual(4350);
     expect(resultArr.length).toBeLessThanOrEqual(4650);
-  })
+  });
 
   it('55% 확률 검증, 오차 범위 1.5%', () => {
     const probability = 55;
     const resultArr = [];
 
     for (let i = 0; i < 10000; i++) {
-      if(gamble(probability)) {
+      if (gamble(probability)) {
         resultArr.push(true);
       }
     }
 
     expect(resultArr.length).toBeGreaterThanOrEqual(5350);
     expect(resultArr.length).toBeLessThanOrEqual(5650);
-  })
+  });
 
   it('65% 확률 검증, 오차 범위 1.5%', () => {
     const probability = 65;
     const resultArr = [];
 
     for (let i = 0; i < 10000; i++) {
-      if(gamble(probability)) {
+      if (gamble(probability)) {
         resultArr.push(true);
       }
     }
 
     expect(resultArr.length).toBeGreaterThanOrEqual(6350);
     expect(resultArr.length).toBeLessThanOrEqual(6650);
-  })
+  });
 
   it('75% 확률 검증, 오차 범위 1.5%', () => {
     const probability = 75;
     const resultArr = [];
 
     for (let i = 0; i < 10000; i++) {
-      if(gamble(probability)) {
+      if (gamble(probability)) {
         resultArr.push(true);
       }
     }
 
     expect(resultArr.length).toBeGreaterThanOrEqual(7350);
     expect(resultArr.length).toBeLessThanOrEqual(7650);
-  })
-})
+  });
+
+  it('능력 요소 중 무작위 3개를 포함하여 배열을 생성 ', () => {
+    const randomAbilities = abilitiesGenerator();
+    const allElementsConatin = randomAbilities.filter((randomAbility) => abilityList.includes(randomAbility));
+    const overlapChecker = randomAbilities.reduce(
+      (abilities: AbilityType[], el: AbilityType) => (abilities.includes(el) ? abilities : [...abilities, el]),
+      []
+    );
+
+    expect(randomAbilities).toHaveLength(3);
+    expect(allElementsConatin).toBeTruthy();
+    expect(randomAbilities).toStrictEqual(overlapChecker);
+  });
+});
