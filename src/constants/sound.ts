3e0de233d46082d04e9f93b734618d6aa53ae904
wonjin-dev <fsdjin159@gmail.com>
2023-01-24 03:02:58 +0900

    :package: gamble 효과음asset 추가

---
+++ src/constants/sound.ts
@@ -0,0 +1,6 @@
+export const SOUNDS = {
+  SUCCESS: '/sound/success.mp3',
+  FAIL: '/sound/fail.mp3',
+  GAMBLE_RESULT_POSITIVE: '/sound/result-positive.mp3',
+  GAMBLE_RESULT_NEGATIVE: '/sound/result-negative.mp3',
+};
