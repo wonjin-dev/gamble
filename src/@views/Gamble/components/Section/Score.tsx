fd36a21726e2f9309d054440e005396d2b81a8d8
wonjin-dev <fsdjin159@gmail.com>
2023-02-16 03:05:59 +0900

    :recycle: 레이아웃 마크업

--- src/@views/Gamble/components/Section/Score.tsx
+++ src/@views/Gamble/components/Section/Score.tsx
@@ -1,28 +1,18 @@
 import styled from '@emotion/styled';
 import {FC} from 'react';
 import {COLORS, rem} from '@styles/theme';
-import useTranslate from '@hooks/useTranslate';
 import {GambleEnchantType} from '@hooks/gamble/useGamble';
-import {checkGambleChance} from '@utils/filters';
 
 interface Props {
   scoreArr: GambleEnchantType[];
 }
 
 const Score: FC<Props> = ({scoreArr}) => {
-  const {translate} = useTranslate();
-  const progress = 10 - checkGambleChance(scoreArr);
-
   return (
     <Container>
       {scoreArr.map((score: GambleEnchantType, index: number) => {
         return <GambleResult key={index} status={score} />;
       })}
-      <ProgressionWrapper isFinish={progress === 10}>
-        <span>
-          {translate('PROGRESSION')}: {progress}/10
-        </span>
-      </ProgressionWrapper>
     </Container>
   );
 };
@@ -45,22 +35,4 @@ const GambleResult = styled.div<{status: GambleEnchantType}>`
   outline: ${({status}) => (status === GambleEnchantType.PENDING ? `${rem(1)} solid ${COLORS.GREY}` : `${rem(1)}`)};
   background-color: ${({status}) => status === GambleEnchantType.SUCCESS && COLORS.AQUA};
   background-color: ${({status}) => status === GambleEnchantType.FAIL && COLORS.GREY};
-`;
-
-const ProgressionWrapper = styled.div<{isFinish: boolean}>`
-  @media (max-width: 510px) {
-    display: none;
-  }
-
-  @media (min-width: 511px) {
-    position: absolute;
-    width: ${rem(90)};
-    right: ${rem(-165)};
-    bottom: 0;
-  }
-
-  span {
-    font-weight: 700;
-    color: ${({isFinish}) => isFinish && COLORS.RED};
-  }
 `;
--- src/@views/Gamble/components/Section/index.tsx
+++ src/@views/Gamble/components/Section/index.tsx
@@ -5,6 +5,7 @@ import {COLORS, rem} from '@styles/theme';
 import {AbilityType, GambleProps, GambleSectionList} from '@hooks/gamble/useGamble';
 import {IMAGES} from '@constants/image';
 import useTranslate from '@hooks/useTranslate';
+import {checkGambleChance} from '@utils/filters';
 import Score from './Score';
 
 interface Props {
@@ -16,6 +17,7 @@ const GambleSection: FC<Props> = ({type, gamble}) => {
   const {translate} = useTranslate();
   const {probability, detail, enchant} = gamble;
   const details = useMemo(() => detail(type), [detail, type]);
+  const progress = useMemo(() => (details ? 10 - checkGambleChance(details.score) : 0), [details]);
 
   const abilityImage = useMemo(() => {
     if (details) {
@@ -51,14 +53,21 @@ const GambleSection: FC<Props> = ({type, gamble}) => {
         <AbilityImageWarpper>
           <AbilityImg src={abilityImage} />
         </AbilityImageWarpper>
+
         <Content>
-          <FlexWrapper>
-            <Ability>{translate(details.ability || 'NULL')}</Ability>
-            {probabilityText}
-          </FlexWrapper>
+          <Ability>{translate(details.ability || 'NULL')}</Ability>
           <Score scoreArr={details.score} />
         </Content>
-        <BaseButton value={translate('GAMBLE')} onClick={() => enchant(type)} width={70} height={32} />
+
+        <ActionContentWrapper>
+          <p className="probability">{probabilityText}</p>
+          <BaseButton value={translate('GAMBLE')} onClick={() => enchant(type)} width={70} height={32} />
+          <ProgressionWrapper isFinish={progress === 10}>
+            <p className="progress">
+              {translate('PROGRESSION')}: {progress} / 10
+            </p>
+          </ProgressionWrapper>
+        </ActionContentWrapper>
       </Container>
     );
   }
@@ -66,11 +75,13 @@ const GambleSection: FC<Props> = ({type, gamble}) => {
   return null;
 };
 
+export default GambleSection;
+
 const Container = styled.div<{isNegativeSection: boolean}>`
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  padding: ${rem(4)};
+  height: ${rem(90)};
+  padding: ${rem(8)};
   outline: ${(props) =>
     props.isNegativeSection ? `${rem(1)} solid ${COLORS.RED}` : `${rem(1)} solid ${COLORS.BLACK}`};
   border-radius: ${rem(6)};
@@ -79,11 +90,18 @@ const Container = styled.div<{isNegativeSection: boolean}>`
 
 const Content = styled.div`
   width: 100%;
-  padding: 0 ${rem(8)};
+  margin: 0 ${rem(6)};
+  margin-left: ${rem(16)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const AbilityImageWarpper = styled.picture`
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const AbilityImg = styled.img`
@@ -94,13 +112,34 @@ const AbilityImg = styled.img`
 `;
 
 const Ability = styled.p`
-  font-size: ${rem(10)};
+  font-size: ${rem(20)};
 `;
 
-const FlexWrapper = styled.div`
+const ActionContentWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: ${rem(2)};
+  width: 100%;
 `;
 
-export default GambleSection;
+const ProgressionWrapper = styled.div<{isFinish: boolean}>`
+  @media (max-width: 510px) {
+    display: none;
+  }
+
+  p {
+    text-align: end;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  .probability {
+    font-weight: 400;
+  }
+
+  .progress {
+    font-weight: 700;
+    color: ${({isFinish}) => isFinish && COLORS.RED};
+  }
+`;
