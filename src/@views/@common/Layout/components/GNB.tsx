f5ffd762c0322f2a80a1a6dbde8e616e23f037fc
wonjin-dev <fsdjin159@gmail.com>
2023-03-03 02:46:36 +0900

    :art: 음소거 토글 버튼 이미지 생성

--- src/@views/@common/Layout/components/GNB.tsx
+++ src/@views/@common/Layout/components/GNB.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import {FC, Fragment} from 'react';
+import {FC, Fragment, useMemo} from 'react';
 import {useRecoilState} from 'recoil';
 import {rem} from '@styles/theme';
 import useTranslate from '@hooks/useTranslate';
@@ -11,6 +11,14 @@ const GNB: FC = () => {
   const {changeLangType} = useTranslate();
   const [muteMode, setMuteMode] = useRecoilState(soundMuteAtom);
 
+  const muteIcon = useMemo(() => {
+    if (muteMode) {
+      return IMAGES.SOUND_MUTE;
+    } else {
+      return IMAGES.SOUND;
+    }
+  }, [muteMode]);
+
   return (
     <Fragment>
       <Container>
@@ -18,7 +26,9 @@ const GNB: FC = () => {
         <LocaleButton onClick={changeLangType}>
           <LocaleButtonImage src={IMAGES.WORLD} alt={'언어 변경 아이콘'} />
         </LocaleButton>
-        <MuteButton onClick={() => setMuteMode(!muteMode)}>TEST</MuteButton>
+        <MuteButton onClick={() => setMuteMode(!muteMode)}>
+          <MuteButtonImage src={muteIcon} alt={'볼륨 아이콘'} />
+        </MuteButton>
       </Container>
       <GradientWrapper>
         <GradientShadow height={10} />
@@ -51,7 +61,17 @@ const LocaleButtonImage = styled.img`
 `;
 
 const MuteButton = styled.div`
+  width: ${rem(15)};
+  height: ${rem(15)};
   cursor: pointer;
+  position: absolute;
+  right: ${rem(38)};
+`;
+
+const MuteButtonImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 `;
 
 const GradientWrapper = styled.div`
