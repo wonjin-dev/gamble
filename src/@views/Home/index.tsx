9944894651edd971d0e49a2f7cbb415a2f6ea673
wonjin-dev <fsdjin159@gmail.com>
2023-03-04 02:45:34 +0900

    :sparkles: 라우트 버튼 추가

--- src/@views/Home/index.tsx
+++ src/@views/Home/index.tsx
@@ -1,7 +1,18 @@
 import {FC} from 'react';
+import {useRouter} from 'next/router';
+import BaseButton from '@views/@common/BaseButton';
+import useTranslate from '@hooks/useTranslate';
+import {ROUTES} from '@constants/routes';
 
 const HomeScreen: FC = () => {
-  return <>Hello, World</>;
+  const {translate} = useTranslate();
+  const router = useRouter();
+
+  return (
+    <>
+      <BaseButton value={translate('GO_TO_GAMBLE')} onClick={() => router.push(ROUTES.GAMBLE)} />
+    </>
+  );
 };
 
 export default HomeScreen;
